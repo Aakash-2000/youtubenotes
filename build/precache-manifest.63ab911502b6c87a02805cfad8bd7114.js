60495e7501cb070e88baef79f9dc2a28283d9c54
Aakash G.S <63035114+Aakash-2000@users.noreply.github.com>
2021-01-03 10:24:10 +0530

    Add files via upload

---
+++ build/precache-manifest.63ab911502b6c87a02805cfad8bd7114.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4892e79c723ac7c5f0fffda6fd986fe8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1735837cdab3c027e882",
+    "url": "/static/css/main.aa85bf03.chunk.css"
+  },
+  {
+    "revision": "01c4d09f5e1285b0dd6c",
+    "url": "/static/js/2.d6260700.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/static/js/2.d6260700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1735837cdab3c027e882",
+    "url": "/static/js/main.8f979181.chunk.js"
+  },
+  {
+    "revision": "b7c7acbc7bdb192699e7",
+    "url": "/static/js/runtime-main.d8a7dc26.js"
+  },
+  {
+    "revision": "b059c9ef074c7f95456e66513aa591c5",
+    "url": "/static/media/YouTube.b059c9ef.png"
+  },
+  {
+    "revision": "ec06c49dfde620fc19c8b510d3f3c7cf",
+    "url": "/static/media/logo.ec06c49d.png"
+  },
+  {
+    "revision": "97c98ef26e8e1c5bc7d69dc7a9802501",
+    "url": "/static/media/main.97c98ef2.jpg"
+  }
+]);
